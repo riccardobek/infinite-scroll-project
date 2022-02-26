f799ad0796cad9f399dd15cdffd2d69e43d040e7
Riccardo <rikibek96@gmail.com>
2022-02-26 16:17:16 +0100

    Exemple of how to use infiniteScrollModule

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,22 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'infinite-scroll-project';
+
+  post: any = [];
+
+  ngOnInit(): void {
+    //Called after the constructor, initializing input properties, and the first call to ngOnChanges.
+    //Add 'implements OnInit' to the class.
+    for(let i= 0; i<100; i++){
+      this.post.push(i);
+    }
+  }
+
+  onScroll(){
+    const length = this.post.length;
+    setTimeout(()=>{
+      const p:any = ' '.repeat(100).split('').map((s,i)=>i+1 + length );
+      while(p.length) this.post.push(p.shift());
+    },1000);
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { InfiniteScrollModule } from 'ngx-infinite-scroll';
+import { NgxSpinnerModule } from 'ngx-spinner';
 
 import { AppComponent } from './app.component';
 
@@ -8,7 +10,9 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    InfiniteScrollModule,
+    NgxSpinnerModule
   ],
   providers: [],
   bootstrap: [AppComponent]
